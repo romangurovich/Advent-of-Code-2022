e1b56d07bda760759fe3923a1d701337474ec723
Roman Gurovich <romangurovich@gmail.com>
2022-12-04 23:50:04 -0800

    day 5

---
+++ 5/5-2/5-2.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	common "Advent-of-Code-2022/Common"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data := common.GetData(5)
+	crates := generateCrates()
+
+	for _, entry := range data[10:] {
+		command := parseCommand(entry)
+		fromColumn := crates[command.From]
+		toColumn := crates[command.To]
+		num := command.NumCrates
+		pivotIndex := len(fromColumn) - num
+		cratesInMotion := fromColumn[pivotIndex:]
+		crates[command.To] = append(toColumn, cratesInMotion...)
+		crates[command.From] = fromColumn[:pivotIndex]
+	}
+
+	fmt.Println(topCrates(crates))
+}
+
+type Command struct {
+	NumCrates int
+	From      int
+	To        int
+}
+
+func topCrates(crates [][]string) string {
+	var result strings.Builder
+	for _, stack := range crates {
+		result.WriteString(stack[len(stack)-1])
+	}
+	return result.String()
+}
+
+func parseCommand(entry string) Command {
+	r, _ := regexp.Compile(`\d+`)
+	matches := r.FindAllString(entry, -1)
+	numCrates, _ := strconv.Atoi(matches[0])
+	from, _ := strconv.Atoi(matches[1])
+	to, _ := strconv.Atoi(matches[2])
+	return Command{numCrates, from - 1, to - 1}
+}
+
+func generateCrates() [][]string {
+	data := common.GetData(5)
+	var allCrates = make([][]string, 9)
+
+	for i := 0; i < 8; i++ {
+		row := data[i]
+		stackIndex := 0
+		for j := 1; j < len(row); j += 4 {
+			val := string(row[j])
+			if val != " " {
+				allCrates[stackIndex] = append([]string{val}, allCrates[stackIndex]...)
+			}
+			stackIndex += 1
+		}
+	}
+
+	return allCrates
+}
